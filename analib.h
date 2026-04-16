aab9ce6ab68ddd2b7de7d218cb5787dcd8fa32d8
Simon Danielsson <116196524+simon-danielsson@users.noreply.github.com>
2026-04-16 23:33:06 +0200

    log function, str function

--- analib.h
+++ analib.h
@@ -1,7 +1,7 @@
 // -------------- Details --------------
 //
 // Name    : Analib
-// Version : 0.1.0
+// Version : 0.1.1
 //
 // Author  : Simon Danielsson
 // Email   : contact@simondanielsson.se
@@ -42,26 +42,81 @@
 #define ANALIB_H_
 
 #include <stdlib.h>
+#include <time.h>
 
 #ifndef ANALIB_DEF
 #define ANALIB_DEF
 #endif // ANALIB_DEF
 
-// an assert that doesn't abort continued execution of your program if false
-#define analib_assert_cont(cond)                                               \
+// DEFINITIONS: DEBUG
+// =============================================================================
+
+// assert that doesn't abort continued execution if false
+#define AL_db_assert(cond)                                                     \
   do {                                                                         \
     if (!(cond)) {                                                             \
-      fprintf(stderr, "FAILED ASSERT: %s (%s:%d)\n", #cond, __FILE__,          \
+      fprintf(stderr, "ASSERT FALSE: %s (%s:%d)\n", #cond, __FILE__,           \
               __LINE__);                                                       \
     }                                                                          \
   } while (0)
 
-ANALIB_DEF int add(int x, int y);
+// formatted log message with file, line and date
+ANALIB_DEF void AL_db_log(const char *msg);
+
+// DEFINITIONS: INTEGERS
+// =============================================================================
+
+// compare two integers and return the bigger one
+ANALIB_DEF int AL_int_max(int a, int b);
+// compare two integers and return the smaller one
+ANALIB_DEF int AL_int_min(int a, int b);
+
+// DEFINITIONS: STRINGS
+// =============================================================================
+
+// get length of str
+ANALIB_DEF int AL_str_get_size(char *s);
 
 #endif // ANALIB_H_
-
 #ifdef ANALIB_IMPLEMENTATION
 
-ANALIB_DEF int add(int x, int y) { return x + y; }
+// IMPLEMENTATIONS: DEBUG
+// =============================================================================
+
+ANALIB_DEF void AL_db_log(const char *msg) {
+  time_t now = time(NULL);
+  struct tm *t = localtime(&now);
+
+  char date[24];
+  snprintf(date, sizeof(date), "%02d-%02d-%d %02d:%02d:%02d", t->tm_mday,
+           t->tm_mon + 1, t->tm_year + 1900, t->tm_hour, t->tm_min, t->tm_sec);
+
+  char header[64];
+  int header_len = snprintf(header, sizeof(header), "----- %s:%d [%s] -----\n",
+                            __FILE_NAME__, __LINE__, date);
+
+  char div[header_len];
+  memset(div, '-', header_len);
+  div[header_len - 1] = '\0';
+
+  fprintf(stdout, "%s%s\n%s\n", header, msg, div);
+}
+
+// IMPLEMENTATIONS: INTEGERS
+// =============================================================================
+
+ANALIB_DEF int AL_int_min(int a, int b) { return a < b ? a : b; }
+
+ANALIB_DEF int AL_int_max(int a, int b) { return a > b ? a : b; }
+
+// IMPLEMENTATIONS: STRINGS
+// =============================================================================
+
+ANALIB_DEF int AL_str_get_size(char *s) {
+  char *t;
+  for (t = s; *t != '\0'; t++)
+    ;
+  return t - s;
+}
 
 #endif // ANALIB_IMPLEMENTATION
--- tests/test.c
+++ tests/test.c
@@ -2,51 +2,14 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 #define ANALIB_IMPLEMENTATION
 #include "../analib.h"
 
-typedef struct {
-    char *pnt;
-} AnaStr;
-
-AnaStr analib_str_new(const char *s) {
-    AnaStr result = {0};
-    if (s) {
-        result.pnt = strdup(s);
-    }
-    return result;
-}
-
-// to lowercase
-void analib_str_lower(AnaStr *s) {
-    if (!s || !s->pnt)
-        return;
-    for (int i = 0; s->pnt[i] != '\0'; i++) {
-        s->pnt[i] = tolower((unsigned char)s->pnt[i]);
-    }
-}
-
-// to uppercase
-void analib_str_upper(AnaStr *s) {
-    if (!s || !s->pnt)
-        return;
-    for (int i = 0; s->pnt[i] != '\0'; i++) {
-        s->pnt[i] = toupper((unsigned char)s->pnt[i]);
-    }
-}
-
-// append analib strings to eachother
-void analib_str_app(AnaStr *a, AnaStr *b_append) {}
-
-char *analib_str_char(AnaStr *s) { return s->pnt; }
-
 int main(void) {
-    // assert(add(6, 6) == 12);
-    AnaStr str = analib_str_new("hello");
-    analib_str_lower(&str);
-    printf("%s\n", analib_str_char(&str));
-    analib_str_upper(&str);
-    printf("%s\n", analib_str_char(&str));
+    char s[] = "hello";
+    printf("%d\n", AL_str_get_size(s));
+    // printf("%d\n", al_min_int(5, 12));
     return 0;
 }
